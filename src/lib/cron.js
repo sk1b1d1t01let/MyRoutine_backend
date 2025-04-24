902407fb1453569b52d4cd134edecc6f017bde42
Ali <crazyanimator4l@gmail.com>
2025-04-24 19:28:43 +0200

    cron

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -2,8 +2,6 @@ import { error } from "console";
 import cron, { CronJob } from "cron"
 import https from "https"
 
-import cron from 'node-cron';
-
 const job = new cron.CronJob('*/14 * * * *', function () {
  https.get(process.env.API_URL, (res) => {
     if(res.statusCode === 200) console.log("GET request sent successfully")
