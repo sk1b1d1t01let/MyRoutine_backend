f39d34a78f4760842d64193daffcd7f6c529ce1c
Ali <crazyanimator4l@gmail.com>
2025-04-25 20:40:14 +0200

    token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,16 +4,19 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+const generateToken = (userId, username, email) => {
+  return jwt.sign(
+    { userId, username, email },  
+    process.env.JWT_SECRET
+  );
 };
+
 
 router.post("/signup", async (req, res) => {
   try {
-    //get user data
+
     const { username, email, password } = req.body;
 
-    //check if the data is ok
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
@@ -30,34 +33,25 @@ router.post("/signup", async (req, res) => {
         .json({ message: "Username must be at least 3 characters long" });
     }
 
-    //check if the email or username already exists
     const existingEmail = await User.findOne({ email: email });
 
     if (existingEmail) {
       return res.status(400).json({ message: "Email already in use" });
     }
 
-    const existingUser = await User.findOne({ username: username });
 
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    //create a new user
     const user = new User({username, email, password});
-    //store in db
+
     await user.save();
 
-    //create jwt
+
     const token = generateToken(user._id);
 
-    //send jwt
+
     res.status(201).json({
       token,
       user: {
-        id: user._id,
         username: user.username,
-        email: user.email,
       },
     });
   } catch (error) {
@@ -87,9 +81,7 @@ router.post("/login", async (req, res) => {
     res.status(200).json({
       token,
       user: {
-        id: user._id,
         username: user.username,
-        email: user.email,
       },
     });
   } catch (error) {
@@ -97,5 +89,32 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+router.post("/token", async (req, res) => {
+  try {
+    const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    return res.status(401).json({message: 'Missing authorisation header'});
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  const verification = jwt.verify(token, process.env.JWT_SECRET)
+
+  if(!verification){
+    return res.status(401).json({message: 'Invalid token'});
+  }
+
+  return res.status(200).json({message: "Authorised" })
+
+
+  } catch (error) {
+    if (error.name === 'JsonWebTokenError') {
+       
+      return res.status(401).json({ message: 'Invalid token' });
+    } else return res.status(500).json({ error: 'Internal server error' });
+  }
+
+})
 
 export default router;
