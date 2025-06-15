9fe9fd490520ccc883d1747008ae3b368b374267
sk1b1d1t01let <crazyanimator4l@gmail.com>
2025-06-15 19:28:21 +0200

    changing gemini model

--- src/functions/generate.js
+++ src/functions/generate.js
@@ -14,7 +14,7 @@ async function generate(input) {
 
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-flash-preview-05-20",
       contents: input,
     });
 
